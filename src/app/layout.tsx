a51aca29179aea6daf83fb9cb684af36c2e612f2
jimiolu <oluwalejimi@gmail.com>
2025-08-01 05:53:19 +0100

    Header update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,9 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "John Doe - Developer",
+  title: "Jimi Oluwale - Developer",
   description:
-    "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+    "Jimi Oluwale | Software Engineer | Developer | Writer | Speaker | Digital Nomad",
 };
 
 export default function RootLayout({
